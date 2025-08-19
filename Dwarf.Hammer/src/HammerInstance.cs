1ac78a1db5bdc029d55fe96362067d3a8b964e89
Patryk Markowski <patrol56789@gmail.com>
2025-08-19 02:06:02 +0200

    coll

--- Dwarf.Hammer/src/HammerInstance.cs
+++ Dwarf.Hammer/src/HammerInstance.cs
@@ -7,9 +7,12 @@ public class HammerInstance {
   public HammerWorld HammerWorld { get; set; }
 
   public delegate void ContactHandler(in BodyId body1, in BodyId body2);
+  public delegate void EmptyContactHandler(in BodyId body1);
+
   public ContactHandler? OnContactAdded;
   public ContactHandler? OnContactPersisted;
   public ContactHandler? OnContactExit;
+  public EmptyContactHandler? OnTilemapContactPersised;
 
   public HammerInstance() {
     HammerWorld = new HammerWorld(this);
